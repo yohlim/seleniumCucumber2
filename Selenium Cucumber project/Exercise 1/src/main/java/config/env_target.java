8395997dd049b5380f2f915612c8c60f76ba134b
Yohana <yohanaclimbong1@gmail.com>
2024-02-25 22:00:24 +0700

    Initial commit

---
+++ Selenium Cucumber project/Exercise 1/src/main/java/config/env_target.java
@@ -0,0 +1,9 @@
+package config;
+
+import org.openqa.selenium.WebDriver;
+
+public class env_target {
+    protected WebDriver driver;
+    protected String baseUrl = "https://www.saucedemo.com/";
+
+}
