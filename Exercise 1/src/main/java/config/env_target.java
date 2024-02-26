d3fe495151f3c48cfced479508e55ded22bbf610
Yohana <58686247+yohlim@users.noreply.github.com>
2024-02-26 20:59:52 +0700

    Delete Exercise 1 directory

--- Exercise 1/src/main/java/config/env_target.java
+++
@@ -1,9 +0,0 @@
-package config;
-
-import org.openqa.selenium.WebDriver;
-
-public class env_target {
-    protected WebDriver driver;
-    protected String baseUrl = "https://www.saucedemo.com/";
-
-}
