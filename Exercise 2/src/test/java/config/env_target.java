102bfe97550a2e6097490d5719324f723e154aec
Yohana <58686247+yohlim@users.noreply.github.com>
2024-02-26 11:22:18 +0700

    Add files via upload

---
+++ Exercise 2/src/test/java/config/env_target.java
@@ -0,0 +1,10 @@
+package config;
+
+import org.openqa.selenium.WebDriver;
+
+
+public class env_target {
+    protected WebDriver driver;
+    protected String parabankLink = "https://parabank.parasoft.com/parabank/index.htm";
+
+}
